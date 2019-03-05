da48f2afbb8a1517fb9511c85e5ea41b302f2a2a
Rifqi31 <muttaqinrifqi31@gmail.com>
2019-03-05 23:22:56 +0700

    add CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// creating route crud
+Route::get('/', 'lightController@index'); 												// index 
+
+Route::get('/lightdata', 'lightController@view_data'); 									// view all data
+Route::post('/lightdata/create', 'lightController@create_data');						// creating process 
+
+Route::get('/lightdata/view/{id}', 'lightController@view_edit_data');					// view selected data
+Route::post('/lightdata/view/edit/{id}', 'lightController@process_edit_data');			// edit process
+
+Route::get('/lightdata/delete/{id}', 'lightController@process_delete_data');			// delete process
